fee01b1eb998ec82fd498bb3374e6aa35a78881f
kesston <40376279+kesston@users.noreply.github.com>
2018-11-16 20:01:27 +0800

    Day04 workshop04

---
+++ Workshop04/workshop04/src/java/workshop04/day04/rest/RESTConfig.java
@@ -0,0 +1,9 @@
+package workshop04.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RESTConfig extends Application {
+    
+}
